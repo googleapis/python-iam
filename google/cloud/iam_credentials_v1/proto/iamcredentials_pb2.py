12cb3128a06b59c3be39f6d21f10c13ab73d34e2
yoshi-automation <yoshi-automation@google.com>
2020-06-23 11:18:23 -0700

    bazel: update protobuf, rules_go, gazelle, and gapic-generator-go versions - protobuf v3.12.1 - rules_go v0.23.0 - gazelle v0.21.0 - gapic-generator-go v0.14.1
    PiperOrigin-RevId: 313460921
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed May 27 14:10:16 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: c4e37010d74071851ff24121f522e802231ac86e
    Source-Link: https://github.com/googleapis/googleapis/commit/c4e37010d74071851ff24121f522e802231ac86e

--- google/cloud/iam_credentials_v1/proto/iamcredentials_pb2.py
+++ google/cloud/iam_credentials_v1/proto/iamcredentials_pb2.py
@@ -24,6 +24,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
     package="google.iam.credentials.v1",
     syntax="proto3",
     serialized_options=b"\n#com.google.cloud.iam.credentials.v1B\023IAMCredentialsProtoP\001ZDgoogle.golang.org/genproto/googleapis/iam/credentials/v1;credentials\370\001\001",
+    create_key=_descriptor._internal_create_key,
     serialized_pb=b'\n:google/cloud/iam_credentials_v1/proto/iamcredentials.proto\x12\x19google.iam.credentials.v1\x1a\x1cgoogle/api/annotations.proto\x1a\x17google/api/client.proto\x1a\x32google/cloud/iam_credentials_v1/proto/common.proto2\xad\x07\n\x0eIAMCredentials\x12\xec\x01\n\x13GenerateAccessToken\x12\x35.google.iam.credentials.v1.GenerateAccessTokenRequest\x1a\x36.google.iam.credentials.v1.GenerateAccessTokenResponse"f\x82\xd3\xe4\x93\x02@";/v1/{name=projects/*/serviceAccounts/*}:generateAccessToken:\x01*\xda\x41\x1dname,delegates,scope,lifetime\x12\xe4\x01\n\x0fGenerateIdToken\x12\x31.google.iam.credentials.v1.GenerateIdTokenRequest\x1a\x32.google.iam.credentials.v1.GenerateIdTokenResponse"j\x82\xd3\xe4\x93\x02<"7/v1/{name=projects/*/serviceAccounts/*}:generateIdToken:\x01*\xda\x41%name,delegates,audience,include_email\x12\xb9\x01\n\x08SignBlob\x12*.google.iam.credentials.v1.SignBlobRequest\x1a+.google.iam.credentials.v1.SignBlobResponse"T\x82\xd3\xe4\x93\x02\x35"0/v1/{name=projects/*/serviceAccounts/*}:signBlob:\x01*\xda\x41\x16name,delegates,payload\x12\xb5\x01\n\x07SignJwt\x12).google.iam.credentials.v1.SignJwtRequest\x1a*.google.iam.credentials.v1.SignJwtResponse"S\x82\xd3\xe4\x93\x02\x34"//v1/{name=projects/*/serviceAccounts/*}:signJwt:\x01*\xda\x41\x16name,delegates,payload\x1aQ\xca\x41\x1diamcredentials.googleapis.com\xd2\x41.https://www.googleapis.com/auth/cloud-platformB\x85\x01\n#com.google.cloud.iam.credentials.v1B\x13IAMCredentialsProtoP\x01ZDgoogle.golang.org/genproto/googleapis/iam/credentials/v1;credentials\xf8\x01\x01\x62\x06proto3',
     dependencies=[
         google_dot_api_dot_annotations__pb2.DESCRIPTOR,
@@ -44,6 +45,7 @@ _IAMCREDENTIALS = _descriptor.ServiceDescriptor(
     file=DESCRIPTOR,
     index=0,
     serialized_options=b"\312A\035iamcredentials.googleapis.com\322A.https://www.googleapis.com/auth/cloud-platform",
+    create_key=_descriptor._internal_create_key,
     serialized_start=197,
     serialized_end=1138,
     methods=[
@@ -55,6 +57,7 @@ _IAMCREDENTIALS = _descriptor.ServiceDescriptor(
             input_type=google_dot_cloud_dot_iam__credentials__v1_dot_proto_dot_common__pb2._GENERATEACCESSTOKENREQUEST,
             output_type=google_dot_cloud_dot_iam__credentials__v1_dot_proto_dot_common__pb2._GENERATEACCESSTOKENRESPONSE,
             serialized_options=b'\202\323\344\223\002@";/v1/{name=projects/*/serviceAccounts/*}:generateAccessToken:\001*\332A\035name,delegates,scope,lifetime',
+            create_key=_descriptor._internal_create_key,
         ),
         _descriptor.MethodDescriptor(
             name="GenerateIdToken",
@@ -64,6 +67,7 @@ _IAMCREDENTIALS = _descriptor.ServiceDescriptor(
             input_type=google_dot_cloud_dot_iam__credentials__v1_dot_proto_dot_common__pb2._GENERATEIDTOKENREQUEST,
             output_type=google_dot_cloud_dot_iam__credentials__v1_dot_proto_dot_common__pb2._GENERATEIDTOKENRESPONSE,
             serialized_options=b'\202\323\344\223\002<"7/v1/{name=projects/*/serviceAccounts/*}:generateIdToken:\001*\332A%name,delegates,audience,include_email',
+            create_key=_descriptor._internal_create_key,
         ),
         _descriptor.MethodDescriptor(
             name="SignBlob",
@@ -73,6 +77,7 @@ _IAMCREDENTIALS = _descriptor.ServiceDescriptor(
             input_type=google_dot_cloud_dot_iam__credentials__v1_dot_proto_dot_common__pb2._SIGNBLOBREQUEST,
             output_type=google_dot_cloud_dot_iam__credentials__v1_dot_proto_dot_common__pb2._SIGNBLOBRESPONSE,
             serialized_options=b'\202\323\344\223\0025"0/v1/{name=projects/*/serviceAccounts/*}:signBlob:\001*\332A\026name,delegates,payload',
+            create_key=_descriptor._internal_create_key,
         ),
         _descriptor.MethodDescriptor(
             name="SignJwt",
@@ -82,6 +87,7 @@ _IAMCREDENTIALS = _descriptor.ServiceDescriptor(
             input_type=google_dot_cloud_dot_iam__credentials__v1_dot_proto_dot_common__pb2._SIGNJWTREQUEST,
             output_type=google_dot_cloud_dot_iam__credentials__v1_dot_proto_dot_common__pb2._SIGNJWTRESPONSE,
             serialized_options=b'\202\323\344\223\0024"//v1/{name=projects/*/serviceAccounts/*}:signJwt:\001*\332A\026name,delegates,payload',
+            create_key=_descriptor._internal_create_key,
         ),
     ],
 )
