69a06be46f7a595d1447228cf9022146203a0c69
SitaLakshmi <sita1996@gmail.com>
2022-07-14 23:39:16 +0530

    added init.py

--- samples/snippets/conftest.py
+++ samples/snippets/conftest.py
@@ -19,8 +19,8 @@ import uuid
 from _pytest.capture import CaptureFixture
 import pytest
 
-import create_deny_policy
-import delete_deny_policy
+from create_deny_policy import create_deny_policy
+from delete_deny_policy import delete_deny_policy
 
 PROJECT_ID = os.environ["GOOGLE_CLOUD_PROJECT"]
 GOOGLE_APPLICATION_CREDENTIALS = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
